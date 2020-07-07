c559e34d2f704e3374ca59b8dbcf6530d81d164c
odeane_croney <odeane_croney@yahoo.com>
2020-07-07 11:52:45 +0200

    adding test for valid factorybot

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
+  it 'should have a valid Factory' do
+    expect(create(:product)).to be_valid
+  end
+  
+
   describe 'Database table' do
     it { is_expected.to have_db_column :name }
     it { is_expected.to have_db_column :description }
