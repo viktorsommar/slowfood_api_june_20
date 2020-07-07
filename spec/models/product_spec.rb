2cca2012c5593ee1486d5b9d675adba24f53136c
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-07 16:29:26 +0200

    Adding a validation test to our model spec. Adding response json to our spec support.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe Product, type: :model do
     it { is_expected.to have_db_column :description }
     it { is_expected.to have_db_column :price }
   end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :price }
+  end
 end
