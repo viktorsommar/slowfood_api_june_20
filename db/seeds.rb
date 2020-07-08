f927e2e2a77ffcb752bd6da068169c5bdd14fd0b
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-08 10:16:07 +0200

    fixed a typo in seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-products = Product.create([{ rname: 'Pokebowl', description: 'fresh', price: 150 }, { name: 'Burger', description: 'juicy', price: 125 }])
+products = Product.create([{ name: 'Pokebowl', description: 'fresh', price: 150 }, { name: 'Burger', description: 'juicy', price: 125 }])
