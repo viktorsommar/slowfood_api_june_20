3fb35a02c25649803840686f0897877efc713d45
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-09 10:15:34 +0200

    amending routes and adding create action to orders controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,4 +1,7 @@
 class Api::OrdersController < ApplicationController
   def create
+    order = Order.create
+    order.order_items.create(product_id: params[:product_id])
+    render json: { message: 'The product has been added to your order', order_id: order.id }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :products, only: [:index], constraints: { format: 'json' }
+      resources :orders, only: [:create]
     end
   end
 end
