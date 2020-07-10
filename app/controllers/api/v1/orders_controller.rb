f0e8b61423a9ec9c35995c3ddbb512c66cbfbbce
Sima <sima.boreiri@gmail.com>
2020-07-10 14:54:56 +0200

    define create action

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::OrdersController < ApplicationController
   def create
-   # order = Order.create
+    order = Order.create(user: current_user)
     #order.order_items.create(product_id: params[:product_id])
     #render json: { message: 'The product has been added to your order', order_id: order.id }
   end
