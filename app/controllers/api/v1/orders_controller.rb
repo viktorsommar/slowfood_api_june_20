b7895c0baa6f179772004b209568126563f18fc3
Sima <sima.boreiri@gmail.com>
2020-07-12 00:16:57 +0200

    formatting

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -8,16 +8,14 @@ class Api::V1::OrdersController < ApplicationController
   def update
     order = current_user.orders.find(params[:id])
     
-    if params[:activity]
-      
+    if params[:activity] 
      order.update_attribute(:finalized, true)
      render json: { message: "Your order will be ready in 30 minutes" }
     else
       product = Product.find(params[:product_id])
       order.order_items.create(product: product)
       render json: create_json_response(order)
-    end
-   
+    end 
   end
 
   private
